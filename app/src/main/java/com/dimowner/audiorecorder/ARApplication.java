9befb703b398cf1438c10edff56b70a069e3209d
hoacomay <levanhauvtn@gmail.com>
2023-06-21 11:55:38 +0700

    init commit

--- app/src/main/java/com/dimowner/audiorecorder/ARApplication.java
+++ app/src/main/java/com/dimowner/audiorecorder/ARApplication.java
@@ -16,14 +16,21 @@
 
 package com.dimowner.audiorecorder;
 
+import android.Manifest;
 import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
 import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyCallback;
 import android.telephony.TelephonyManager;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 
 import com.dimowner.audiorecorder.audio.player.PlayerContractNew;
 import com.dimowner.audiorecorder.data.Prefs;
@@ -34,109 +41,151 @@ import timber.log.Timber;
 
 public class ARApplication extends Application {
 
-	final static String AUDIO_BECOMING_NOISY = "android.media.AUDIO_BECOMING_NOISY";
-	private AudioOutputChangeReceiver audioOutputChangeReceiver;
+    final static String AUDIO_BECOMING_NOISY = "android.media.AUDIO_BECOMING_NOISY";
+    private AudioOutputChangeReceiver audioOutputChangeReceiver;
 
-	private static String PACKAGE_NAME ;
-	public static volatile Handler applicationHandler;
+    private static String PACKAGE_NAME;
+    public static volatile Handler applicationHandler;
 
-	/** Screen width in dp */
-	private static float screenWidthDp = 0;
+    /**
+     * Screen width in dp
+     */
+    private static float screenWidthDp = 0;
 
-	public static Injector injector;
+    public static Injector injector;
 
-	public static Injector getInjector() {
-		return injector;
-	}
+    public static Injector getInjector() {
+        return injector;
+    }
 
-	public static String appPackage() {
-		return PACKAGE_NAME;
-	}
+    public static String appPackage() {
+        return PACKAGE_NAME;
+    }
 
-	/**
-	 * Calculate density pixels per second for record duration.
-	 * Used for visualisation waveform in view.
-	 * @param durationSec record duration in seconds
-	 */
-	public static float getDpPerSecond(float durationSec) {
-		if (durationSec > AppConstants.LONG_RECORD_THRESHOLD_SECONDS) {
-			return AppConstants.WAVEFORM_WIDTH * screenWidthDp / durationSec;
-		} else {
-			return AppConstants.SHORT_RECORD_DP_PER_SECOND;
-		}
-	}
+    /**
+     * Calculate density pixels per second for record duration.
+     * Used for visualisation waveform in view.
+     *
+     * @param durationSec record duration in seconds
+     */
+    public static float getDpPerSecond(float durationSec) {
+        if (durationSec > AppConstants.LONG_RECORD_THRESHOLD_SECONDS) {
+            return AppConstants.WAVEFORM_WIDTH * screenWidthDp / durationSec;
+        } else {
+            return AppConstants.SHORT_RECORD_DP_PER_SECOND;
+        }
+    }
 
-	public static int getLongWaveformSampleCount() {
-		return (int)(AppConstants.WAVEFORM_WIDTH * screenWidthDp);
-	}
+    public static int getLongWaveformSampleCount() {
+        return (int) (AppConstants.WAVEFORM_WIDTH * screenWidthDp);
+    }
 
-	@Override
-	public void onCreate() {
-		if (BuildConfig.DEBUG) {
-			//Timber initialization
-			Timber.plant(new Timber.DebugTree() {
-				@Override
-				protected String createStackElementTag(StackTraceElement element) {
-					return "AR-AR " + super.createStackElementTag(element) + ":" + element.getLineNumber();
-				}
-			});
-		}
-		super.onCreate();
+    private boolean callStateListenerRegistered = false;
 
-		PACKAGE_NAME = getApplicationContext().getPackageName();
-		applicationHandler = new Handler(getApplicationContext().getMainLooper());
-		screenWidthDp = AndroidUtils.pxToDp(AndroidUtils.getScreenWidth(getApplicationContext()));
-		injector = new Injector(getApplicationContext());
-		Prefs prefs = injector.providePrefs();
-		if (!prefs.isMigratedSettings()) {
-			prefs.migrateSettings();
-		}
+    @Override
+    public void onCreate() {
+        if (BuildConfig.DEBUG) {
+            //Timber initialization
+            Timber.plant(new Timber.DebugTree() {
+                @Override
+                protected String createStackElementTag(StackTraceElement element) {
+                    return "AR-AR " + super.createStackElementTag(element) + ":" + element.getLineNumber();
+                }
+            });
+        }
+        super.onCreate();
 
-		IntentFilter intentFilter = new IntentFilter();
-		intentFilter.addAction(AUDIO_BECOMING_NOISY);
-		audioOutputChangeReceiver = new AudioOutputChangeReceiver();
-		registerReceiver(audioOutputChangeReceiver, intentFilter);
+        PACKAGE_NAME = getApplicationContext().getPackageName();
+        applicationHandler = new Handler(getApplicationContext().getMainLooper());
+        screenWidthDp = AndroidUtils.pxToDp(AndroidUtils.getScreenWidth(getApplicationContext()));
+        injector = new Injector(getApplicationContext());
+        Prefs prefs = injector.providePrefs();
+        if (!prefs.isMigratedSettings()) {
+            prefs.migrateSettings();
+        }
 
-		try {
-			TelephonyManager mTelephonyMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-			mTelephonyMgr.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
-		} catch (Exception e) {
-			Timber.e(e);
-		}
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(AUDIO_BECOMING_NOISY);
+        audioOutputChangeReceiver = new AudioOutputChangeReceiver();
+        registerReceiver(audioOutputChangeReceiver, intentFilter);
+
+        try {
+            TelephonyManager mTelephonyMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                    mTelephonyMgr.registerTelephonyCallback(getMainExecutor(), callStateListener);
+                    callStateListenerRegistered = true;
+                }
+            } else {
+                mTelephonyMgr.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+                callStateListenerRegistered = true;
+            }
+            mTelephonyMgr.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
 //		FirebaseApp.initializeApp(this);
-	}
+    }
 
-	@Override
-	public void onTerminate() {
-		super.onTerminate();
-		//This method is never called on real Android devices
-		injector.releaseMainPresenter();
-		injector.closeTasks();
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        //This method is never called on real Android devices
+        injector.releaseMainPresenter();
+        injector.closeTasks();
 
-		unregisterReceiver(audioOutputChangeReceiver);
-	}
+        unregisterReceiver(audioOutputChangeReceiver);
+    }
 
-	private static class AudioOutputChangeReceiver extends BroadcastReceiver {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String actionOfIntent = intent.getAction();
-			if (actionOfIntent != null && actionOfIntent.equals(AUDIO_BECOMING_NOISY)){
-				PlayerContractNew.Player player = injector.provideAudioPlayer();
-				player.pause();
-			}
-		}
-	}
+    private static class AudioOutputChangeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String actionOfIntent = intent.getAction();
+            if (actionOfIntent != null && actionOfIntent.equals(AUDIO_BECOMING_NOISY)) {
+                PlayerContractNew.Player player = injector.provideAudioPlayer();
+                player.pause();
+            }
+        }
+    }
 
-	private PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
-		@Override
-		public void onCallStateChanged(int state, String incomingNumber) {
-			if ((state == TelephonyManager.CALL_STATE_RINGING)
-					|| (state == TelephonyManager.CALL_STATE_OFFHOOK)) {
-				//Pause playback when incoming call or on hold
-				PlayerContractNew.Player player = injector.provideAudioPlayer();
-				player.pause();
-			}
-			super.onCallStateChanged(state, incomingNumber);
-		}
-	};
+    private CallStateListener callStateListener = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ?
+            new CallStateListener() {
+                @Override
+                public void onCallStateChanged(int state) {
+                    // Handle call state change
+                }
+            }
+            : null;
+
+    private PhoneStateListener phoneStateListener = (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) ?
+            new PhoneStateListener() {
+                @Override
+                public void onCallStateChanged(int state, String phoneNumber) {
+                    if ((state == TelephonyManager.CALL_STATE_RINGING)
+                            || (state == TelephonyManager.CALL_STATE_OFFHOOK)) {
+                        //Pause playback when incoming call or on hold
+                        PlayerContractNew.Player player = injector.provideAudioPlayer();
+                        player.pause();
+                    }
+                }
+            }
+            : null;
+//    private PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
+//        @Override
+//        public void onCallStateChanged(int state, String incomingNumber) {
+//            if ((state == TelephonyManager.CALL_STATE_RINGING)
+//                    || (state == TelephonyManager.CALL_STATE_OFFHOOK)) {
+//                //Pause playback when incoming call or on hold
+//                PlayerContractNew.Player player = injector.provideAudioPlayer();
+//                player.pause();
+//            }
+//            super.onCallStateChanged(state, incomingNumber);
+//        }
+//    };
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private static abstract class CallStateListener extends TelephonyCallback implements TelephonyCallback.CallStateListener {
+        @Override
+        abstract public void onCallStateChanged(int state);
+    }
 }
